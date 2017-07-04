f37c0b98eed91a46231b2140b043087887cbdeaf
Maxime Daniel <root@maxux.net>
2017-07-04 10:52:58 +0200

    Early fix of tmux management

--- lib/tmux.sh
+++ lib/tmux.sh
@@ -12,27 +12,49 @@ the previous running command will be killed (this is done by killing the window)
 
 EOF
 }
+
 TMUX_Start() {(
-    #CHECK that tmux is installed, if not properly show
     echo '' > $ZLogFile
+    if ! which tmux 2>&1 > /dev/null; then
+        die "tmux not found"
+    fi
+
     local session="main"
     local name="main"
     local command=""
     local keep=0
     local OPTIND
+    local extra=""
+
     while getopts "n:c:s:hk" opt; do
         case $opt in
            n )  name=$OPTARG ;;
            c )  command=$OPTARG ;;
            s )  session=$OPTARG ;;
-           k )  keep = 1 ;;
+           k )  keep=1 ;;
            h )  TMUX_Start_Usage ; return 0 ;;
         esac
-
-        shift
     done
-    if  [ "$command" != "" ] ; then
-        die "IMPLEMENT"
+
+    if  [ "$command" != "" ]; then
+        # if session doesn't exists, nothing exists, just spawn it
+        if ! tmux has-session -t $session 2>&1 > /dev/null; then
+            tmux new-session -s $session -n $name $command
+            return 0
+        fi
+
+        # Now, we know the session already exists, let's check if window
+        # already exists
+        if [ $keep -eq 0 ]; then
+            extra="-k"
+        fi
+
+        # FIXME: need to select right session
+
+        if tmux list-window 2>&1 | grep -q " $name "; then
+            tmux new-window $extra -n $name $command
+        fi
+
     else
         die "could not start tmux session, check syntax: $@"
     fi
@@ -49,24 +71,26 @@ Usage: ZEXEC [-l]
 
 EOF
 }
+
 TMUX_Stop() {(
-    #CHECK that tmux is installed, if not properly show
     echo '' > $ZLogFile
+    if ! which tmux 2>&1 > /dev/null; then
+        die "tmux not found"
+    fi
+
     local name="main"
     local session="main"
     local OPTIND
+
     while getopts "n:s:h" opt; do
         case $opt in
            n )  name=$OPTARG ;;
            s )  session=$OPTARG ;;
            h )  TMUX_Stop_Usage ; return 0 ;;
         esac
-        shift
     done
-    if  [ "$command" != "" ] ; then
-        die "IMPLEMENT"
-    else
-        die "could not kill tmux session, check syntax: $@"
-    fi
+
+    # FIXME: need session handling
+    tmux kill-window -t $name
 
 )}
