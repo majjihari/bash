c639aa63f2185ce5e2008862aae8d00ee5b34377
Abdelrahman Hussein <ahussein@codescalers.com>
2018-05-09 13:13:13 +0200

    update travis prepare script

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -3,10 +3,8 @@ set -e
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 export SSHKEYNAME=id_rsa
-
+echo "Using JS9BRANCH: ${JS9BRANCH}"
 sudo -HE bash install.sh
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZCodeGetJS"
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZDockerInstallLocal"
-eval $(ssh-agent)
-ssh-add
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZInstall_js9_full"
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZCodeGetJS"
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerInstallLocal"
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZInstall_js9_full"
