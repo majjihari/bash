8cfb83d78211e6cb690a31976ffcfb5f965d2d9e
Abdelrahman Hussein <ahussein@codescalers.com>
2018-05-09 16:08:37 +0200

    remove debug msg

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -6,7 +6,6 @@ export SSHKEYNAME=id_rsa
 
 export ZUTILSBRANCH=${ZUTILSBRANCH:-master}
 export JS9BRANCH=${JS9BRANCH:-master}
-echo "Using JS9BRANCH: ${JS9BRANCH}"
 
 sudo -E bash install.sh
 sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZCodeGetJS"
